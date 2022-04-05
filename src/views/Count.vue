bbeea1b4cfb51e1fe700ca9bc7216e3fabc67fdf
ryota-09 <n.ryo950911@gmail.com>
2022-04-06 07:58:34 +0900

    参考サイトの記述

--- src/views/Count.vue
+++ src/views/Count.vue
@@ -5,6 +5,8 @@
       <h1>provide / injectの使い方</h1>
       <h3>~ Vuexを使用しない状態管理 ~</h3>
       <p>例として、あえて入れ子構造にしていく。</p>
+      <a href="https://zenn.dev/okakyo/articles/f39d5c2c328b6d">参考サイト１ </a>
+      <a href="https://tech.012grp.co.jp/entry/2021/02/09/094209">参考サイト２ </a>
       <br />
       <CountUp />
     </div>
